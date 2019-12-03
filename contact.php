78de6e7e5d529a1424876f75100c8f43e89fddb6
Mr-Personne <h.sacha07@gmail.com>
2019-12-03 15:33:47 +0000

    mise en place de base en terme de style? (base du portfolio en place, contact basique en place, progess bar bootstrap page skills

--- contact.php
+++ contact.php
@@ -1,37 +1,34 @@
 <section class="contact">
-  <div class="container-fluid pl-5 pr-5 fade-anim">
+  <div class="container-fluid p-5 fade-anim">
     <div class="container">
       <div class="row">
         <div class="col-3">
-          <h2>Sacha</h2>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, omnis dolores
-            laudantium excepturi maiores, modi aliquam sit nisi recusandae assumenda est. Labore
-            sint quod blanditiis voluptate dignissimos officia perferendis cum.</p>
+          <h2>Lorem? Lorem!</h2>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, omnis dolores
             laudantium excepturi maiores, modi aliquam sit nisi recusandae assumenda est. Labore
             sint quod blanditiis voluptate dignissimos officia perferendis cum.</p>
         </div>
 
         <div class="col-9">
-          <form action="traitement.php" method="post" class="needs-validation" novalidate>
+          <form action="" method="post" class="needs-validation" novalidate>
             <div class="row w-100 mx-auto">
               <div class="col-6">
-                <input type="text" class="form-control py-4" placeholder="Full Name" id="validationTooltip01" required
+                <input type="text" class="form-control mb-4" placeholder="Full Name" id="validationTooltip01" required
                   name="name">
                 <span class="text-danger"></span>
               </div>
               <div class="col-6">
-                <input type="email" class="form-control  py-4" placeholder="Your Email" required name="email">
+                <input type="email" class="form-control  mb-4" placeholder="Your Email" required name="email">
               </div>
             </div>
             <div class="row w-100 mx-auto">
               <div class="col-12">
-                <input type="text" class="form-control py-4" placeholder="Your Title" required name="title">
+                <input type="text" class="form-control mb-4" placeholder="Your Title" required name="title">
               </div>
             </div>
             <div class="row w-100 mx-auto">
               <div class="col-12">
-                <textarea class="form-control py-4" id="validationTextarea" rows="7" placeholder="Your Comment" required
+                <textarea class="form-control mb-4" id="validationTextarea" rows="7" placeholder="Your Comment" required
                   name="comment"></textarea>
               </div>
             </div>
--- portfolio.php
+++ portfolio.php
@@ -1,17 +1,21 @@
 <section class="portfolio">
-            <div class="container-fluid p-5 fade-anim">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <h2>Sacha</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, omnis dolores
-                                laudantium excepturi maiores, modi aliquam sit nisi recusandae assumenda est. Labore
-                                sint quod blanditiis voluptate dignissimos officia perferendis cum.</p>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, omnis dolores
-                                laudantium excepturi maiores, modi aliquam sit nisi recusandae assumenda est. Labore
-                                sint quod blanditiis voluptate dignissimos officia perferendis cum.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+  <div class="container-fluid p-5 fade-anim">
+    <div class="container">
+      <div class="row pb-1 justify-content-center">
+        <div class="col-5 text-center bg-work portfolio-dimensions w-sm-100 m-1">
+
+        </div>
+        <div class="col-5 text-center bg-work portfolio-dimensions w-sm-100 m-1">
+
+        </div>
+        <div class="col-5 text-center bg-work portfolio-dimensions w-sm-100 m-1">
+
+        </div>
+        <div class="col-5 text-center bg-work portfolio-dimensions w-sm-100 m-1">
+
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</section>
