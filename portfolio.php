b5407ed99c8a3e07b7ebe01b88711f5c1d107765
Mr-Personne <h.sacha07@gmail.com>
2019-12-03 18:36:42 +0000

    changed portfolio images from background-image (in div) to img tag inside div (with link to websites)

--- portfolio.php
+++ portfolio.php
@@ -2,16 +2,19 @@
   <div class="container-fluid p-5 fade-anim">
     <div class="container">
       <div class="row pb-1 justify-content-center">
-        <div class="col-5 text-center bg-work portfolio-dimensions w-sm-100 m-1">
+        <div class="col-5 text-center portfolio-dimensions w-sm-100 m-1">
+            <a href="https://sachah.promo-vesoul33.codeur.online/file-browser/" target="_blank"><img src="media/project1.png" alt="project 1 image" width="1601" height="757"></a>
+        </div>
+        <div class="col-5 text-center portfolio-dimensions w-sm-100 m-1">
+            <a href="https://sachah.promo-vesoul33.codeur.online/restaurant/" target="_blank"><img src="media/project2.png" alt="project 2 image" width="1582" height="757"></a>
 
         </div>
-        <div class="col-5 text-center bg-work portfolio-dimensions w-sm-100 m-1">
+        <div class="col-5 text-center portfolio-dimensions w-sm-100 m-1">
+            <a href="https://sachah.promo-vesoul33.codeur.online/php-pirolldesign/" target="_blank"><img src="media/project3.png" alt="project 3 image" width="1582" height="759"></a>
 
         </div>
-        <div class="col-5 text-center bg-work portfolio-dimensions w-sm-100 m-1">
-
-        </div>
-        <div class="col-5 text-center bg-work portfolio-dimensions w-sm-100 m-1">
+        <div class="col-5 text-center portfolio-dimensions w-sm-100 m-1">
+            <a href="https://sachah.promo-vesoul33.codeur.online/puissance4/" target="_blank"><img src="media/project4.png" alt="project 4 image" width="1582" height="757"></a>
 
         </div>
       </div>
